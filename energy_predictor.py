e3a6d5ea0b9cf2ae8b7f4be3367690b373d4964b
Ramon Moscatelli <ramon.moscatelli@ost.ch>
2022-11-12 17:46:13 +0100

    Update

--- energy_predictor.py
+++ energy_predictor.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 
 if __name__ == "__main__":
@@ -15,41 +16,63 @@ if __name__ == "__main__":
     raw_data_wetter = req_wetter.json()
     sonnenscheindauer_tagessumme = [entry["fields"]["sre000d0"] / 60 if entry["fields"]["sre000d0"] > 0 else 1 for entry in raw_data_wetter["records"]]
     globalstrahlung_tagesmittel = [entry["fields"]["gre000d0"] if entry["fields"]["gre000d0"] > 0 else 1 for entry in raw_data_wetter["records"]]
-    energiedichte = [a*b for a, b in zip(sonnenscheindauer_tagessumme, globalstrahlung_tagesmittel)]
+    energiedichte_sonnenstrahlung = [a * b for a, b in zip(sonnenscheindauer_tagessumme, globalstrahlung_tagesmittel)]
     print('Globalstahlung Tagesmittel: ', globalstrahlung_tagesmittel)
     print('Sonnenscheindauer Tagessumme: ', sonnenscheindauer_tagessumme)
-    print('Energiedichte: ', energiedichte)
+    print('Energiedichte: ', energiedichte_sonnenstrahlung)
 
     # Solaranlagen Daten
     data_frame = pd.read_csv('stromproduktion-der-solaranlagen-der-stgaller-stadtwerke2.csv', delimiter=';')
-    data_frame = data_frame.loc[:, ['DateTime (Local Time)', 'Name', 'Additional Energy Export']]
+    data_frame = data_frame.loc[:, ['DateTime (Local Time)', 'Name', 'Additional Energy Export', 'Fläche in m2']]
     data_frame['DateTime (Local Time)'] = data_frame['DateTime (Local Time)'].str.split('T').str[0]
     data_frame = data_frame.dropna()
     data_frame = data_frame.sort_values(by=['DateTime (Local Time)'])
-    leistung = []
+    energiedichte_solaranlagen = []
     old_date = data_frame['DateTime (Local Time)'][0]
     my_sum = 0
     for index, row in data_frame.iterrows():
         if row['DateTime (Local Time)'] == old_date:
-            my_sum += row['Additional Energy Export']
+            my_sum += row['Additional Energy Export'] / row['Fläche in m2']
         else:
             old_date = row['DateTime (Local Time)']
-            leistung.append(my_sum)
+            energiedichte_solaranlagen.append(my_sum)
             my_sum = 0
     #leistung.append(my_sum)
-    print('Leistung: ', leistung)
+    print('Energiedichte Solaranlagen: ', energiedichte_solaranlagen)
 
     # Lineare Regression
-    model = LinearRegression().fit(np.array(energiedichte).reshape([-1, 1]), leistung)
+    model = LinearRegression().fit(np.array(energiedichte_sonnenstrahlung).reshape([-1, 1]), energiedichte_solaranlagen)
     b0 = model.intercept_
     b1 = model.coef_
-    print(b0, b1)
+    r2 = model.score(np.array(energiedichte_sonnenstrahlung).reshape([-1, 1]), energiedichte_solaranlagen)
+    print('R^2-Wert: ', r2)
+
+    # Polinomische Regression
+    n = 4
+    X = np.array(energiedichte_sonnenstrahlung).reshape([-1, 1])
+    poly = PolynomialFeatures(degree=n)
+    X_poly = poly.fit_transform(X)
+    poly.fit(X_poly, energiedichte_solaranlagen)
+    lin2 = LinearRegression()
+    lin2.fit(X_poly, energiedichte_solaranlagen)
 
     # Plot
     fig, ax = plt.subplots(constrained_layout=True)
-    ax.scatter(energiedichte, leistung)
-    ax.plot(energiedichte, b0 + b1*leistung)
+    xi = np.linspace(np.min(energiedichte_sonnenstrahlung), np.max(energiedichte_sonnenstrahlung), 10)
+    line = xi * b1 + b0
+    plt.plot(energiedichte_sonnenstrahlung, energiedichte_solaranlagen, 'o')
+    plt.plot(xi, line, label='lineare Regression')
+    plt.plot(xi, lin2.predict(poly.fit_transform(np.array(xi).reshape([-1, 1]))), label=f'polinomische Regression (n={n})')
     plt.xlabel("Energiedichte der Sonnenstahlung [$Wh/m^2$]")
-    plt.ylabel("Energie aus den Solaranlagen [$Wh$]")
+    plt.ylabel("Energiedichte aus den Solaranlagen [$Wh/m^2$]")
+    plt.legend()
     plt.grid()
     plt.show()
+
+    # Predictor
+    print('\nSolarenergie Vorhersage')
+    print('--------------------------------')
+    sonnenscheindauer_input = float(input('Bitte geben Sie die Sonnenscheindauer [h] für einen Tag ein: '))
+    globalstrahlung_input = float(input('Bitte geben Sie die durchschnittliche Globalstrahlung für einen Tag ein: '))
+    energiedichte_output = float(sonnenscheindauer_input*globalstrahlung_input*b1 + b0)
+    print(f'Geschätzte Energiedichte der Solaranlagen der St. Galler Stadtwerke: {0.001*energiedichte_output:.3f} [kWh/m^2]')
